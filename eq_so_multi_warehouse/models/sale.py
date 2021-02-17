7a32762b204706313124b7aa522a311745b29093
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-02-17 15:35:59 +0100

    Update sale.py

--- eq_so_multi_warehouse/models/sale.py
+++ eq_so_multi_warehouse/models/sale.py
@@ -25,7 +25,7 @@ class sale_order(models.Model):
 class sale_order_line(models.Model):
     _inherit = 'sale.order.line'
 
-    sol_warehouse_id = fields.Many2one('stock.warehouse', string="Warehouse")
+    sol_warehouse_id = fields.Many2one('stock.warehouse', string="Warehouseid")
 
     @api.depends('product_id', 'customer_lead', 'product_uom_qty', 'product_uom', 'order_id.warehouse_id', 'sol_warehouse_id', 'order_id.commitment_date')
     def _compute_qty_at_date(self):
