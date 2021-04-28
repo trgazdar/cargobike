ee1bb1692b8f2ae12a694e9572550f0364e4f240
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-04-28 16:49:15 +0200

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -95,8 +95,7 @@ class StockPicking(models.Model):
                             product_code = product_supplier.product_code
                         elif move_line.product_id.default_code:
                             product_code = move_line.product_id.default_code
-                        total_objets = picking_id.move_lines.search_count([('product_uom_qty', '>', 0), ( 'picking_id','=', move_line.picking_id[0])])#picking_id.id 
-                        data = {
+                        total_objets = picking_id.move_lines.search_count([('product_uom_qty', '>', 0), ( 'picking_id','=', move_line.picking_id[0])])
                             '1': commande,
                             'EL': 'L',
                             'CBD': 'CBD',
