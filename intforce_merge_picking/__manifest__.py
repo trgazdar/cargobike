8a49e4f5cf0ac2d11c1f798c4b544f47e8abd37c
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-04-28 15:58:02 +0200

    add merge picking

---
+++ intforce_merge_picking/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': "Merge Multiple Delivery Orders / Incoming Shipments",
+    'summary': """This module will support you to merge multiple delivery orders / incoming shipments in single picking""",
+    'description': """
+    Merge Any Type of Picking 
+    Merge Incoming Shipment
+    Merge Delivery Orders
+    """,
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    "license" : "Other proprietary",
+    'images': ['static/description/banner.jpg'],
+    'category': 'Uncategorized',
+
+    'depends': ['base','stock'],
+
+    'data': [
+        'views/merge_picking.xml'
+       
+    ],
+  
+}
