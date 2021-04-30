4a674340a6a518b40c7f9c7b269d13c36c3ec7d5
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-04-30 17:33:42 +0200

    Update stock_picking_view.xml

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -10,6 +10,7 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
     _order = 'partner_id'
     is_exported = fields.Boolean(string="Is Exported?", default=False)
+    is_blocked = fields.Boolean(string="Is Blocked?", default=False)
 
     def export_shipment_orders_to_ftp(self, pickings=False, partner_ids=False):
         """
