066616d3c2524bd1b0a824a07e824a55eec5ff83
moujik74 <yoannguillemot@orange.fr>
2021-05-05 19:29:41 +0200

    add login to yuba

---
+++ login_to_yuba/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+     "name": "Login to Yubabikes",
+    "summary": "Adaptation Yubabikes aux processus de connection",
+    "version": "13.0.1.0.0",
+    "category": "Web",
+    "website": "https://www.yubabikes.com",
+    "author": "Yoann Guillemot",
+    "depends" : ['website','website_sale'],
+    "data": [
+        'views/assest.xml',
+        'views/template.xml',
+        'views/website_category.xml',
+    ],
+    'qweb' : [],
+    "auto_install": False,
+    "installable": True,
+    "category" : "eCommerce",
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ login_to_yuba/static/src/js/website_category_page_app.js
@@ -0,0 +1,10 @@
+odoo.define('website_minimum_quantity_app.quantity', function (require) {
+'use strict';
+	var rpc = require('web.rpc');
+	var ajax = require('web.ajax');
+	$(document).ready(function() {
+		$(".login_page").click(function(){
+			$('.access_denied').hide()
+		});
+	});
+});
