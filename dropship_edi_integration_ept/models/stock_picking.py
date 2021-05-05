1377885e2fd5ad33036c148b3250972544a23ee0
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-05-05 18:01:21 +0200

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -66,9 +66,10 @@ class StockPicking(models.Model):
                         commande = commande + 1
                         #if order_not_matched:
                         #   continue
-                        self.env.cr.execute('select count(*) from stock_move_line where picking_id=%s ', (picking_id.id[0]))
+                        #self.env.cr.execute('select count(*) from stock_move_line where picking_id=%s ', (picking_id.id[0]))
+                        results = self.env['stock.move.line'].search([('picking_id', '!=', picking_id.id)])
                         #total_objets2 = picking_id.move_lines.search_count([('product_qty', '!=', 0), ( 'picking_id','=', picking_id.id)]) 
-                        total_objets2 = self.env.cr.fetchone()
+                        total_objets2 = len(results)
                         data = {
                                 '1': commande,
                                 'EL': 'E',
