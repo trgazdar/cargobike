64f17baa3657a8cf1458e342142c32e1d8a39c4d
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-05-10 15:33:56 +0200

    state

--- intforce_merge_picking/models/merge_picking.py
+++ intforce_merge_picking/models/merge_picking.py
@@ -11,13 +11,14 @@ class purchase_order(models.Model):
         ('confirmed', 'Waiting Availability'),
         ('partially_available', 'Partially Available'),
         ('assigned', 'Available'),
-        ('sent', 'sent to carrier'),
+        ('sent', 'Sent to carrier'),
         ('done', 'Done')], string='Status',
         copy=False, default='draft', index=True, readonly=True,
         help="* New: When the stock move is created and not yet confirmed.\n"
              "* Waiting Another Move: This state can be seen when a move is waiting for another one, for example in a chained flow.\n"
              "* Waiting Availability: This state is reached when the procurement resolution is not straight forward. It may need the scheduler to run, a component to be manufactured...\n"
              "* Available: When products are reserved, it is set to \'Available\'.\n"
+             "* sent to carrier: When CSV file sent to, it is set to \'Available\'.\n"
              "* Done: When the shipment is processed, the state is \'Done\'.")
 
 class StockPicking(models.Model):
