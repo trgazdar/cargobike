0474d82edd9929a51ddb76c870be54d82eca1530
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-05-11 09:52:45 +0200

    MAJ report forecast

--- stock_report_quantity_by_location/__manifest__.py
+++ stock_report_quantity_by_location/__manifest__.py
@@ -1,11 +1,9 @@
-# Copyright 2019-21 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Stock Report Quantity By Location",
-    "summary": "Stock Report Quantity By Location",
+    "name": "Stock Report Quantity By Location and override Forecast view",
+    "summary": "Stock Report Quantity By Location and forecast override",
     "version": "13.0.1.0.0",
-    "author": "ForgeFlow, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/stock-logistics-reporting",
+    "author": "Tripode-Services",
     "category": "Warehouse Management",
     "license": "AGPL-3",
     "depends": ["product", "stock"],
--- stock_report_quantity_by_location/readme/CONTRIBUTORS.rst
+++
@@ -1,6 +0,0 @@
-* ForgeFlow, S.L. (https://www.forgeflow.com)
-  * Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
-  * Hector Villarreal <hector.villarreal@forgeflow.com>
-
- * Ecosoft (http://ecosoft.co.th)
-   * Kranokporn Thongdoung <kranokpornt@ecosoft.co.th>
