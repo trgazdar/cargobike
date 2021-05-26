c5fa33758bad94c993a1232adb2c399cd8a4c6c1
moujik74 <yoannguillemot@orange.fr>
2021-05-26 23:00:24 +0200

    correct connected user

--- login_to_yuba/controller/controllers.py
+++ login_to_yuba/controller/controllers.py
@@ -4,13 +4,6 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 from odoo.addons.portal.controllers.web import Home
 from odoo import http, models, fields, _
 
-class ForceLogin(WebsiteSale):
-    @route()
-    def shop(self):
-        if not request.session.uid:
-            return request.redirect('/web/login?redirect=/shop')
-        return super(ForceLogin, self).shop()
-
 class Website(Home):
 
     @http.route('/', type='http', auth="public", website=True)
