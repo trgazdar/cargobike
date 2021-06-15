7a2555228e13cf8724d17e2fe16e81a785dd7f2c
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-06-15 11:36:42 +0200

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -133,7 +133,7 @@ class StockPicking(models.Model):
                                           " path. File has not exported to Supplier's FTP." %
                                           (partner_id.name)})
                 buffer.seek(0)
-                file_data = buffer.read().encode('iso-8859-1')
+                file_data = buffer.read().encode('iso-8859-1', 'ignore')
                 if file_data:
                     vals = {
                         'name': filename,
