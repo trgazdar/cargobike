7a340af6e8b0fed240c40c66dd30b7b4e3859478
Divyesh <divyesh.kukadiya@konsultoo.com>
2021-10-26 11:41:36 +0530

    [ADD] add module to merge product.

---
+++ deltatech_merge/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Merge Objects",
+    "summary": "Base module for merging",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Tools",
+    "depends": ["base"],
+    "license": "LGPL-3",
+    "data": ["security/merge_security.xml",
+    "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
