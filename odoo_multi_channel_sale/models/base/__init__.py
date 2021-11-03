c260dfb25f618e95d542c412a1788b81364d7c30
jrodery <48557329+jrodery@users.noreply.github.com>
2021-11-03 14:28:37 -0700

    step 2 multi commerce

---
+++ odoo_multi_channel_sale/models/base/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# Copyright (c) 2015-Present Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# See LICENSE file for full copyright and licensing details.
+# License URL : <https://store.webkul.com/license.html/>
+##############################################################################
+
+from . import extra_categories
+from . import multi_channel_sale
+from . import multi_channel_skeleton
+from . import channel_order_states
