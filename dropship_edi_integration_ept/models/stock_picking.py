af48b9c4b9bf9c2cb71bb072b355224a292c41a9
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-11-15 18:03:44 +0100

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -469,7 +469,7 @@ class StockPicking(models.Model):
                                 #on appelle la fonction de SWAP des Num lot
                                 self.swap_num_lot(csvwriter, job, stock_lot_id.id, ids_returned[0], order_ref_prev)
                                 self.env.cr.execute("update stock_move_line set qty_done = 1 where lot_id = " + str(stock_lot_id.id) + " and reference = '" + str(order_ref_prev) +"'" )
-                                log_message = 'REF : ' + str(product_ref_prev) + ' - SN : ' + str(stock_lot_id.name)
+                                log_message = 'REF : ' + str(product_ref_prev) + ' - SNSWAP : ' + str(stock_lot_id.name)
                                 self._create_common_log_line(job, csvwriter, log_message)
 
                     else:    
