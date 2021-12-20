56f72d416fb78ab7e2e43c74cf96883eaa3a421c
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-12-20 11:03:21 +0100

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -440,7 +440,7 @@ class StockPicking(models.Model):
                                                    ('state', 'not in', ['done', 'cancel'])],
                                                   limit=1)
                         _logger.info(str(stock_pickng_id.name))
-                        _logger.info(str(stock_pickng_id))
+                        _logger.info(str(stock_pickng_id[0].id))
                         stock_picking_ids.unlink(stock_pickng_id[0].id)
                         
                         order_ref_prev = order_ref
