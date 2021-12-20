5e59af055fad6bc2cdca891aa0863c1a9dbaae81
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-12-20 11:49:58 +0100

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -437,7 +437,7 @@ class StockPicking(models.Model):
                     #Gestion de la première ligne ECTRA
                     if str(product_qty) == 'E':
                         #del lot_existants[:]
-                        stock_pickng_id = self.search([('name', '=', order_ref),
+                        stock_pickng_id = self.search([('name', '=', order_ref)],
                                                   limit=1)
                         _logger.info(str(stock_pickng_id.name))
                         if stock_pickng_id:
