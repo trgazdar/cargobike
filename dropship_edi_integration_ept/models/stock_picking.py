b8a73eb453852b96c91cd027c010cad24f4f2b93
cargobikedistribution <64523798+cargobikedistribution@users.noreply.github.com>
2021-12-23 18:29:09 +0100

    Update stock_picking.py

--- dropship_edi_integration_ept/models/stock_picking.py
+++ dropship_edi_integration_ept/models/stock_picking.py
@@ -528,11 +528,16 @@ class StockPicking(models.Model):
                 
 
                 for validate_picking_id in list(set(validate_picking_ids)):
-                    tracking_no = validate_picking_id.carrier_tracking_ref
-                    validate_picking_id.action_done()
-                    log_message = (_("Dropship order validated successfully."))
-                    self._create_common_log_line(job, csvwriter, log_message,
-                                                 validate_picking_id.origin, tracking_no)
+                    try:
+                        tracking_no = validate_picking_id.carrier_tracking_ref
+                        validate_picking_id.action_done()
+                        log_message = (_("Delivery validated successfully : " + str(validate_picking_id.name)))
+                        self._create_common_log_line(job, csvwriter, log_message,
+                                                    validate_picking_id.origin, tracking_no)
+                    except:
+                        log_message = (_("ERROR : " + str(validate_picking_id.name)))
+                        self._create_common_log_line(job, csvwriter, log_message,
+                                                    validate_picking_id.origin, tracking_no)
 
                 file = open(filename)
                 file.seek(0)
