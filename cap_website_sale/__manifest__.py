ac5003e35259f726f0c97d5f4760b7f53991bbb1
Julie Rossignol <77502966+captivea-jro@users.noreply.github.com>
2021-12-23 16:04:04 +0100

    Upload pricelist module

---
+++ cap_website_sale/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "cap_website_sale",
+    "version": "1.1",
+    "author": "Captivea",
+    "summary": "Modifications du site e-commerce",
+    "depends": ["website_sale"],
+    "installable": True
+}
---
+++ cap_website_sale/controllers/website_sale_form.py
@@ -0,0 +1,23 @@
+import logging
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+from odoo import http
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteSaleForceAuth(WebsiteSale):
+
+    def _get_pricelist_context(self):
+        """
+            Surcharge de la méthode standard pour exploiter la liste de prix de l'utilisateur si définie
+        """
+        pricelist_context, pricelist = super()._get_pricelist_context()
+
+        context = dict(request.env.context)
+        if not context.get('pricelist'):
+            if request.env.user.partner_id.property_product_pricelist:
+                pricelist_context['pricelist'] = request.env.user.partner_id.property_product_pricelist.id
+
+        return pricelist_context, pricelist
